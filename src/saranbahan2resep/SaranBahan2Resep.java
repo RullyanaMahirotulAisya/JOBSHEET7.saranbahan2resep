06cc591bef7482501c837c30f5949261ae4da32f
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-03 13:08:36 +0700

    Initial Commit

---
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saranbahan2resep;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class SaranBahan2Resep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String identitas = "Rullyana Mahirotul Aisya / XRPL5 / 38";
+         System.out.println("Identitas : "+ identitas);
+         
+          System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+          
+          
+    }
+    
+}
